ebd1a8dab9f66e5a86639350de80ab2f0d74af19
typhoon <jungwooshin.97@gmail.com>
2025-04-16 15:14:11 +0900

    (update)plug-in,system clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "y", "y+", { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "p", '"*p', { desc = "Paste from system clipboard" })
