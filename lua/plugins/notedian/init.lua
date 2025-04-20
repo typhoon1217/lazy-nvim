bdf67e204fa3f275b87a949537a3c04a44541a6e
typhoon <jungwooshin.97@gmail.com>
2025-04-20 16:39:12 +0900

    (feat)add keymap for notesidian

--- lua/plugins/notedian/init.lua
+++ lua/plugins/notedian/init.lua
@@ -8,4 +8,34 @@ return {
   opts = {
     notes_root = "$NOTES",
   },
+  keys = {
+    {
+      "<leader>od",
+      function()
+        require("notesidian").create_daily_note()
+      end,
+      desc = "Create or edit daily note",
+    },
+    {
+      "<leader>ol",
+      function()
+        require("notesidian").create_todo_list()
+      end,
+      desc = "Create or edit Todo List",
+    },
+    {
+      "<leader>oc",
+      function()
+        require("notesidian").toggle_checkbox()
+      end,
+      desc = "Toggle Todo List Checkbox",
+    },
+    {
+      "<leader>of",
+      function()
+        require("notesidian").find_notes()
+      end,
+      desc = "Search Notes",
+    },
+  },
 }
