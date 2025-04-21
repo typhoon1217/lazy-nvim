fd07c5ef7f24a0e3ccf84a2a3c4eb29aace7030a
typhoon <jungwooshin.97@gmail.com>
2025-04-22 02:06:46 +0900

    (fix)import order

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,9 +19,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "lazyvim.plugins.extras.dap.core" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- lua/plugins/dashboard/init.lua
+++ lua/plugins/dashboard/init.lua
@@ -1,27 +1,43 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      opts.config.center = {
-        {
-          action = "Telescope oldfiles",
-          desc = " Recent files",
-          icon = " ",
-          key = "r",
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  opts = function()
+    local logo = [[
+    ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+    ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+    ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+    ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+    ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+    ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+    ]]
+
+    local opts = {
+      theme = "doom",
+      hide = {
+        statusline = false,
+        tabline = false,
+        winbar = false,
+      },
+      config = {
+        header = vim.split(logo, "\n"),
+        center = {
+          { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+          { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+          { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+          { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
+          { action = "lua require('lazyvim.util').telescope.config_files()", desc = " Config", icon = " ", key = "c" },
+          { action = "LazyExtras", desc = " Extras", icon = " ", key = "e" },
+          { action = "Lazy", desc = " Plugins", icon = "󰒲 ", key = "p" },
+          { action = "qa", desc = " Quit", icon = " ", key = "q" },
         },
-        {
-          action = "Telescope projects",
-          desc = " Projects",
-          icon = " ",
-          key = "p",
-        },
-        {
-          action = "qa",
-          desc = " Quit",
-          icon = " ",
-          key = "q",
-        },
-      }
-    end,
-  },
+        footer = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+        end,
+      },
+    }
+
+    return opts
+  end,
 }
